dcdb52e1c0f422b9399a9f4f683f98d8218eb274
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-01-20 17:25:01 -0800

    Add is_learner_in_class_of method

--- class_tree/natural_tree/models.py
+++ class_tree/natural_tree/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Collection(MPTTModel):
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+    type = models.CharField(max_length=50)
 
 
 class User(models.Model):
+    roles = models.ManyToManyField(Collection, through='Role', related_query_name='roles')
+
     def is_learner_in_class_of(self, user):
-        pass
+        classes = Collection.objects.filter(role__user=user, role__type="coach", type="classroom")
+        learners = classes.get_descendants().filter(role__user=self, role__type="learner")
+        return any(learners)
+
+
+class Role(models.Model):
+    type = models.CharField(max_length=50)
+    collection = models.ForeignKey(Collection)
+    user = models.ForeignKey(User)
--- class_tree/natural_tree/tests.py
+++ class_tree/natural_tree/tests.py
@@ -6,6 +6,25 @@ from django.test import TestCase
 from .models import *
 
 
+class TestSanity(TestCase):
+    """
+    Just checks that the User.is_learner_in_class_of method works as expected.
+    """
+    def setUp(self):
+        classroom = Collection.objects.create(type="classroom")
+        lg = Collection.objects.create(type="learner_group", parent=classroom)
+        coach_user, learner_user = self.coach, self.learner = User.objects.create(), User.objects.create()
+
+        coach_role = Role.objects.create(user=coach_user, collection=classroom, type="coach")
+        learner_role = Role.objects.create(user=learner_user, collection=lg, type="learner")
+
+    def test_true(self):
+        self.assertTrue(self.learner.is_learner_in_class_of(self.coach))
+
+    def test_false(self):
+        self.assertFalse(self.coach.is_learner_in_class_of(self.learner))
+
+
 class TestBenchmark(TestCase):
     def test_is_learner_timing(self):
         random.seed(42)
